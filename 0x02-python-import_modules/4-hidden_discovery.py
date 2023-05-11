db37ff789380cba96db4af88c0936eeda20406ea
GeebeeFrost <omgbolahan1@gmail.com>
2023-05-11 14:57:16 +0000

    Add program to print names in hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4, glob
+    hid = []
+    for name in dir(hidden_4):
+        if "__" not in name:
+            hid.append(name)
+    for name in hid:
+        print(name)
