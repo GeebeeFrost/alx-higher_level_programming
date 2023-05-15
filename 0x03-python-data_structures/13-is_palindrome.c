c01723b30f3be91fd86357aaa66366098686b781
GeebeeFrost <omgbolahan1@gmail.com>
2023-05-15 17:21:20 +0000

    Add C function to check if linked list is a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Address of pointer to head node
+ *
+ * Return: 1 if it is a palindrome or 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+
+	return (0);
+}
