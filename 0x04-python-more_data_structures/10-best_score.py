21750bad1eab6411621ba01fa06524a6e1336d83
GeebeeFrost <omgbolahan1@gmail.com>
2023-05-17 16:22:32 +0000

    Correct 10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Returns the key with the biggest integer value"""
-    if not a_dictionary:
+    if a_dictionary == None:
         return None
     if len(a_dictionary) == 1:
         return list(a_dictionary.values())[0]
