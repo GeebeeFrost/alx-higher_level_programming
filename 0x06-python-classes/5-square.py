eb0a943bd8e404416c9c70f854732a790cd4e61f
GeebeeFrost <omgbolahan1@gmail.com>
2023-06-01 14:04:27 +0000

    Add class method to print square object

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Implementation of Square class"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Constructs a new square
+        Args:
+            size(int, optional): size of the Square object
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of a square object"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Computes the area of a Square object
+        Returns:
+            The area of a Square object
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints a square object using # characters"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end='')
+                print()
