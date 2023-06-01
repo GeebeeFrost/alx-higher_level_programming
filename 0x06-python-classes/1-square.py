15760b5ddcc57323a6e35677e99aa4a68e77f8b4
GeebeeFrost <omgbolahan1@gmail.com>
2023-06-01 13:02:18 +0000

    Add implementation of Square class with private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Implementation of Square class"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Constructor method
+
+        Args:
+            size: size of Square instance
+        """
+        self.__size = size
