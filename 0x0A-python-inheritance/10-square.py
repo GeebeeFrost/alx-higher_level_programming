62e333fbc2f9c7d3339d879bd6aad5ecbbafc193
GeebeeFrost <omgbolahan1@gmail.com>
2023-06-12 15:13:42 +0000

    Add subclass Square inheriting from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains the implementation of the Square class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square"""
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
