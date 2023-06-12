bf0c5e7c0e4f0d3a518ad8bc5fed2066a8460804
GeebeeFrost <omgbolahan1@gmail.com>
2023-06-12 14:04:44 +0000

    Correct 4-inherits_from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
         obj: object to be checked
         a_class: class to be checked for
     """
-    return (issubclass(type(obj), a_class))
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
