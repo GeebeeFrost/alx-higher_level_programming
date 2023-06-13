34e05d221ca766860a6b180fdbfb4e7c4aaf57bf
GeebeeFrost <omgbolahan1@gmail.com>
2023-06-13 14:54:18 +0000

    Add script to add arguments to Python list and serialize to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+if __name__ == "__main__":
+    ls = sys.argv[1:]
+    try:
+        items = load("add_item.json")
+    except:
+        save(ls, "add_item.json")
+    else:
+        for arg in ls:
+            items.append(arg)
+        save(items, "add_item.json")
