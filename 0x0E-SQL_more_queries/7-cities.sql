c38a1a5db5942a7e2bdf5d69d72027b2cf57b970
GeebeeFrost <omgbolahan1@gmail.com>
2023-07-20 05:12:51 +0000

    Add query to create database and table with foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- creates table cities with foreign key
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+);
