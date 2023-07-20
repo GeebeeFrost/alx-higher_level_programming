3ea1e77a744d63ba34529595385915ce7d634aad
GeebeeFrost <omgbolahan1@gmail.com>
2023-07-20 11:27:26 +0000

    Correct 7-cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
