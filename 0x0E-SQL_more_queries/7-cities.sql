23077546607949824f72d3e016e6853f5c730c4a
GeebeeFrost <omgbolahan1@gmail.com>
2023-07-20 09:51:55 +0000

    Correct 7-cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- creates table cities with foreign key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
