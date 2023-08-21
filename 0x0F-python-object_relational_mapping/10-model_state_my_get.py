104609603bb3074510f014e750ac7b935281edf1
GeebeeFrost <omgbolahan1@gmail.com>
2023-08-21 03:28:13 +0000

    Add script that prints State object matching argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script lists all State objects from the database hbtn_0e_6_usa"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    eng_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+            )
+    engine = create_engine(eng_url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
