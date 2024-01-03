56b51a0b51393c6ceb7e662d0515280180511173
GeebeeFrost <omgbolahan1@gmail.com>
2024-01-03 14:50:25 +0000

    Add JS script to get webpage contents and store in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request.get(url, (err, response, body) => {
+  if (err) return console.error(err);
+  fs.writeFile(filename, body, (error) => {
+    if (error) return console.error(error);
+  });
+});
