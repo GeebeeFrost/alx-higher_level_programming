5051eb604466b5ddbc0106f2b74d709c335f54a9
GeebeeFrost <omgbolahan1@gmail.com>
2024-01-10 12:27:37 +0100

    Add JS script that fetches translation on Enter press

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $("INPUT#language_code").keypress((key) => {
+    if (key.keyCode == 13) {
+      const code = $("INPUT#language_code").val();
+      $.get("https://hellosalut.stefanbohacek.dev/?lang=" + code, (res) => {
+        $("DIV#hello").text(res.hello);
+      });
+    }
+  });
+});
